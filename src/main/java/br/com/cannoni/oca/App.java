c0db139e63be3bfea19ffc91306ccbd535b2b8f1
Patrizio Cannoni <patrizio.cannoni@gmail.com>
2022-08-26 07:27:55 -0300

    Streams

--- src/main/java/br/com/cannoni/oca/App.java
+++ src/main/java/br/com/cannoni/oca/App.java
@@ -3,29 +3,26 @@ package br.com.cannoni.oca;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Function;
 
 public class App {
+    
     public static void main(String[] args) {
         List<String> palavras = new ArrayList<>();
         palavras.add("alura online");
         palavras.add("casa do código");
         palavras.add("caelum");
-
-        Comparator<String> c = (s1, s2) -> Integer.compare(s1.length(), s2.length());
-
-        Comparator<String> c1 = Comparator.comparing(s -> s.length());
-
-        Function<String, Integer> f = s -> s.length();        
-        Comparator<String> c2 = Comparator.comparing(f);
-
-        Comparator<String> c3 = Comparator.comparing(String::length);
         
-        palavras.sort(c1);
+        palavras.sort(Comparator.comparing(String::length));
+        palavras.forEach(System.out::println);
 
-        palavras.forEach(s -> System.out.println(s));
+        System.out.println("----");
 
-        new Thread(() -> System.out.println("Executando um Runnable")).start();
+        palavras.sort(Comparator.naturalOrder());
+        palavras.forEach(System.out::println);
+
+        System.out.println("----");
+
+        palavras.stream().filter(s -> s.length() > 6).forEach(System.out::println);
     }
     
 }
